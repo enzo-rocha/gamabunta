5dee6ad8f908a1fa2b5bdf0ef390e197cbdd93f2
ENZO.ROCHA <enzo.rocha@lince.com.br>
2024-07-15 17:12:48 -0300

    ...

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gorilla/websocket"
 	"log"
+	"net"
 	"net/http"
 )
+
+var clients = make(map[*websocket.Conn]bool)
 
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
@@ -14,57 +18,61 @@ var upgrader = websocket.Upgrader{
 	},
 }
 
-func reader(conn *websocket.Conn) {
+const myMachineIP = "YOUR_IP_ADDRESS" // Replace with your actual IP address
+
+func main() {
+	http.HandleFunc("/ws", echo)
+	http.HandleFunc("/", homePage)
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Printf("Error in [ListenAndServe]: %v", err)
+		return
+	}
+}
+
+func echo(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Printf("Error in [Upgrade]: %v", err)
+		return
+	}
+
+	clients[conn] = true
+	defer func() {
+		delete(clients, conn)
+		conn.Close()
+	}()
+
+	ip, _, err := net.SplitHostPort(r.RemoteAddr)
+	if err != nil {
+		log.Printf("Error in [SplitHostPort]: %v", err)
+		return
+	}
+	userName := "Guigas"
+	if ip == myMachineIP {
+		userName = "Enzo"
+	}
+
 	for {
 		messageType, message, err := conn.ReadMessage()
-		if err != nil {
-			log.Printf("Error in [conn.ReadMessage]: %v", err)
+		if err != nil || messageType == websocket.CloseMessage {
+			log.Printf("Error in [ReadMessage]: %v", err)
 			return
 		}
 
-		err = conn.WriteMessage(messageType, message)
-		if err != nil {
-			log.Printf("Error in [WriteMessage]: %v", err)
-			return
+		fullMessage := fmt.Sprintf("%s: %s", userName, string(message))
+		for client := range clients {
+			err := client.WriteMessage(websocket.TextMessage, []byte(fullMessage))
+			if err != nil {
+				log.Printf("Error in [WriteMessage]: %v", err)
+				client.Close()
+				delete(clients, client)
+			}
 		}
-
 	}
 }
 
 func homePage(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "index.html")
-}
-
-func ws(w http.ResponseWriter, r *http.Request) {
-	conn, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		log.Printf("Error in [upgrader.Upgrade]: %v", err)
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-
-	log.Println("===================================")
-	log.Println("== CLIENT SUCCESSFULLY CONNECTED ==")
-	log.Println("===================================")
-
-	reader(conn)
-}
-
-func setupRoutes() {
-	http.HandleFunc("/", homePage)
-	http.HandleFunc("/ws", ws)
-}
-
-func main() {
-	setupRoutes()
-
-	log.Println("=======================")
-	log.Println("== RUNNING SERVER... ==")
-	log.Println("=======================")
-
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		log.Fatalf("Error in [ListenAndServe]: %v", err)
-		return
-	}
 }
